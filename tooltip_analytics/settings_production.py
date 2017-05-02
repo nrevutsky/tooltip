92333eeee81ab953b4a3302d58f43857d355bfe3
kolya_terletskyi <mykola.terletskyi@synergy-way.com>
2017-05-02 13:25:34 +0300

    add database production settings

---
+++ tooltip_analytics/settings_production.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tooltip',
+        'USER': 'tooltip_user',
+        'PASSWORD': 'tooltippass',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
