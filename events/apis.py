5e8da70b162ad79ef19bc461229ddc121d0c4d56
kolya_terletskyi <mykola.terletskyi@synergy-way.com>
2017-05-03 18:58:45 +0300

    rewrite redis to save data per hours

--- events/apis.py
+++ events/apis.py
@@ -5,16 +5,24 @@ from django.core.cache import cache
 from .tasks import save_events_count_to_db
 from rest_framework import status
 from .utils import get_project_analytics_by_period, get_general_data, get_message_analytics_by_period
+import datetime as dt
 
 
 @api_view(['POST'])
 def receive_events_data(request):
     if request.data:
         if request.data.get('secret') == settings.SECRET_KEY_API:
-            if not cache.get(request.data.get('event_type')):
+            event_date = dt.datetime.fromtimestamp(request.data.get('time_stamp')).replace(minute=0, second=0)
+            events_general = cache.get('events_general')
+            if not events_general:
+                events_general = {}
+            events_by_hour = events_general.get(event_date)
+            if not events_by_hour:
+                events_by_hour = {request.data.get('event_type'): {request.data.get('project_id'): {request.data.get('message_id'): 0}}}
+            if not events_by_hour.get(request.data.get('event_type')):
                 event_by_project_dict = {request.data.get('project_id'): {request.data.get('message_id'): 0}}
-                cache.set(request.data.get('event_type'), event_by_project_dict)
-            event = cache.get(request.data.get('event_type'))
+                events_by_hour[request.data.get('event_type')] = event_by_project_dict
+            event = events_by_hour[request.data.get('event_type')]
             event_project = event.get(request.data.get('project_id'))
             if event_project:
                 event_project_msg = event_project.get(request.data.get('message_id'))
@@ -25,9 +33,11 @@ def receive_events_data(request):
                 event[request.data.get('project_id')] = event_project
             else:
                 event[request.data.get('project_id')] = {request.data.get('message_id'): 1}
-            cache.set(request.data.get('event_type'), event)
-            save_events_count_to_db()
-            return Response(status=status.HTTP_200_OK)
+            events_by_hour[request.data.get('event_type')] = event
+            events_general[event_date] = events_by_hour
+            cache.set('events_general', events_general)
+            # save_events_count_to_db()
+            return Response(status=status.HTTP_200_OK, data=cache.get('events_general')[event_date])
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
--- events/tasks.py
+++ events/tasks.py
@@ -9,19 +9,28 @@ EVENTS = ['visit', 'trigger', 'click', 'close', 'goal']
 
 # @periodic_task(run_every=dt.timedelta(seconds=300))
 def save_events_count_to_db():
-    for event_name in EVENTS:
-        event = cache.get(event_name)
-        if event:
-            values_to_delete = {}
-            for project_id, project_body in event.iteritems():
-                for message_id, message_count in project_body.iteritems():
-                    save_to_event_db(project_id, message_id, message_count, event_name)
-                    values_to_delete[message_id] = project_id
-            for msg_id, pr_id in values_to_delete.iteritems():
-                del event.get(pr_id)[msg_id]
-                if not event.get(pr_id):
-                    del event[pr_id]
-            cache.set(event_name, event)
+    general_events = cache.get('events_general')
+    empty_hours = []
+    for hour in general_events:
+        for event_name in EVENTS:
+            event = general_events[hour].get(event_name)
+            if event:
+                values_to_delete = {}
+                for project_id, project_body in event.iteritems():
+                    for message_id, message_count in project_body.iteritems():
+                        save_to_event_db(project_id, message_id, message_count, event_name, hour)
+                        values_to_delete[message_id] = project_id
+                for msg_id, pr_id in values_to_delete.iteritems():
+                    del event.get(pr_id)[msg_id]
+                    if not event.get(pr_id):
+                        del event[pr_id]
+                    if not event:
+                        del general_events[hour][event_name]
+        if not general_events[hour]:
+            empty_hours.append(hour)
+    for empty_hour in empty_hours:
+        del general_events[empty_hour]
+    cache.set('events_general', general_events)
 
 
 def save_event(msg, event, count):
@@ -37,12 +46,12 @@ def save_event(msg, event, count):
         msg.close += count if count else 0
 
 
-def save_to_event_db(pr_id, msg_id, msg_count, event_name):
+def save_to_event_db(pr_id, msg_id, msg_count, event_name, date_event):
     current_day = dt.datetime.now().day
-    msg, created = DayEvent.objects.get_or_create(hour=dt.datetime.now().hour,
+    msg, created = DayEvent.objects.get_or_create(hour=date_event.hour,
                                                   project_id=pr_id,
                                                   message_id=msg_id)
-    msg_day, created_day = PeriodEvent.objects.get_or_create(day=dt.date.today(),
+    msg_day, created_day = PeriodEvent.objects.get_or_create(day=date_event.date(),
                                                              project_id=pr_id,
                                                              message_id=msg_id)
     msg_general, created_general = GeneralEvent.objects.get_or_create(project_id=pr_id,
