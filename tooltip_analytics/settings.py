8e3de6ae15714ee341e11e9ef829c6e097ead125
kolya_terletskyi <mykola.terletskyi@synergy-way.com>
2017-05-08 10:48:11 +0300

    add CORP

--- tooltip_analytics/settings.py
+++ tooltip_analytics/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'events'
+    'events',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'tooltip_analytics.urls'
